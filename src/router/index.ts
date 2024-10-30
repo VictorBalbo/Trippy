8e47e8010b5008a75bcb6432d01de9d9b764f46b
Victor Balbo <victorbalbo@gmail.com>
2024-10-29 22:38:57 -0300

    initial commit

---
+++ src/router/index.ts
@@ -0,0 +1,16 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import PlanView from '@/views/PlanView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    { path: '/', redirect: { name: 'Plan' } },
+    {
+      path: '/plan',
+      name: 'Plan',
+      component: PlanView
+    },
+  ]
+})
+
+export default router
